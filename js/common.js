dd27115c546a627f11f65ca6f0cf70333e39e369
Anton Shaman <anton.shaman2015@gmail.com>
2020-02-16 17:37:32 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,18 @@
+var switchBtn = document.getElementById('switch-btn');
+var switchWrapBtn = document.querySelector('.switch-wrap__btn');
+        switchBtn.onclick = function() {
+        switchWrapBtn.classList.toggle('switchIsActive');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
